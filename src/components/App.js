697b6691e1c69e5063793a3da3b036a6f4e78398
ilona <ilona.avdiushkina@gmail.com>
2021-06-17 15:01:13 +0200

    fetching new componnents

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import FindMiddle from "./FindMiddle";
-
-import "./App.css";
+import './App.css';
+import Nav from './Nav';
+import InputContainer from './InputContainer';
+import InfoContainer from './InfoContainer';
+import Footer from './Footer';
 
 function App() {
-    return (
-        <div className="App">
-            <FindMiddle />
-        </div>
-    );
+  return (
+    <div className="App">
+      <p>Hello World and Siski!</p>
+      < Nav />
+      < InputContainer />
+      < InfoContainer />
+      < Footer />
+    </div>
+  );
 }
 
 export default App;
