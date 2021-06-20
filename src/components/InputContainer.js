739cbf8238ea103ac649490890d5f900f3897e88
Mamuna <mamuna_anwar@hotmail.com>
2021-06-20 22:18:07 +0200

    cloning and npm install

--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -2,38 +2,38 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 
 import InputField from "./InputField";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        "& > *": {
-            margin: theme.spacing(1),
-        },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
     },
+  },
 }));
 
 function InputContainer() {
-    const classes = useStyles();
-    return (
-        <div className="InputContainer">
-            <Container maxWidth="sm">
-                <form className={classes.root} noValidate autoComplete="off">
-                    <InputField person="A" />
-                    <InputField person="B" />
-                    <Fab color="primary" aria-label="add">
-                        <AddIcon />
-                    </Fab>
-                    <Button variant="contained" color="primary">
-                        Find middle point
-                    </Button>
-                    {/* loop for others */}
-                </form>
-            </Container>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <div className="InputContainer">
+      <Container maxWidth="sm">
+        <form className={classes.root} noValidate autoComplete="off">
+          <InputField person="A" />
+          <InputField person="B" />
+          <Fab color="primary" aria-label="add">
+            <AddIcon />
+          </Fab>
+          <Button variant="contained" color="primary">
+            Find middle point
+          </Button>
+          {/* loop for others */}
+        </form>
+      </Container>
+    </div>
+  );
 }
 
 export default InputContainer;
