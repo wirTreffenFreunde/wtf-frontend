6a0343d0f536e4a9a7e0c03f101bfcc134e0c618
Mamuna <mamuna_anwar@hotmail.com>
2021-06-21 11:49:15 +0200

    working on fetching

--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
@@ -16,21 +17,55 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function InputContainer() {
+  // input fields for everyone
+  const [peopleAddresses, setPeopleAddresses] = useState([]);
+  // middle point
+  const [middlePoint, setMiddlePoint] = useState("");
+
+  // saving all the input fields
+  const handleChangeMiddle = (e) => {
+    setPeopleAddresses({
+      ...peopleAddresses,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmitMiddle = async (e) => {
+    e.preventDefault();
+    console.log(peopleAddresses);
+    const result = await axios.post(
+      `http://localhost:8080/api`,
+      peopleAddresses
+    );
+    console.log(result.data);
+    setMiddlePoint(result.data);
+  };
+
   const classes = useStyles();
   return (
     <div className="InputContainer">
       <Container maxWidth="sm">
-        <form className={classes.root} noValidate autoComplete="off">
-          <InputField person="A" />
-          <InputField person="B" />
+        <form
+          className={classes.root}
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmitMiddle}
+        >
+          <InputField person="A" onChange={handleChangeMiddle} />
+          <InputField person="B" onChange={handleChangeMiddle} />
           <Fab color="primary" aria-label="add">
             <AddIcon />
           </Fab>
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSubmitMiddle}
+          >
             Find middle point
           </Button>
           {/* loop for others */}
         </form>
+        {middlePoint && <p>{middlePoint}</p>}
       </Container>
     </div>
   );
