3f3eddac28299a2abe13ad4fae66688d2e4dbf76
RalucaStefania <raluca@bochis.com>
2021-06-22 13:20:43 +0200

    started working on my account

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
-import './App.css';
-import Nav from './Nav';
-import InputContainer from './InputContainer';
-import InfoContainer from './InfoContainer';
-import Footer from './Footer';
+import "./App.css";
+import Nav from "./Nav";
+import InputContainer from "./InputContainer";
+import InfoContainer from "./InfoContainer";
+import Footer from "./Footer";
+import MyAccount from "./MyAccount";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      < Nav />
-      < InputContainer />
-      < InfoContainer />
-      < Footer />
+      <Nav />
+      <InputContainer />
+      <InfoContainer />
+      <Footer />
+      <MyAccount />
+
+      <Router>
+        <Switch>
+          <Route path="/myAccount">
+            <MyAccount />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/MyAccount.js
@@ -0,0 +1,3 @@
+export default function MyAccount() {
+  return "My Account";
+}
