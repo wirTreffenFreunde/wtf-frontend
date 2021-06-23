ac6b9c03e5ef219e965a1bf712ad0a9e6cd41d6e
Mamuna Anwar <mamuna_anwar@hotmail.com>
2021-06-23 10:23:26 +0200

    fighting with map

--- src/components/App.js
+++ src/components/App.js
@@ -13,71 +13,76 @@ import "./App.css";
 import { AppData } from "../app-data-context";
 
 function App() {
-    // input fields for everyone
-    const [peopleAddresses, setPeopleAddresses] = useState([]);
-    // middle point
-    const [middlePoint, setMiddlePoint] = useState("");
+  // input fields for everyone
+  const [peopleAddresses, setPeopleAddresses] = useState([]);
+  // middle point
+  const [middlePoint, setMiddlePoint] = useState("");
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  // saving all the input fields
+  const handleChangeMiddle = (e) => {
+    setPeopleAddresses({
+      ...peopleAddresses,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    // saving all the input fields
-    const handleChangeMiddle = (e) => {
-        setPeopleAddresses({
-            ...peopleAddresses,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const handleSubmitMiddle = async (e) => {
-        e.preventDefault();
-        console.log(peopleAddresses);
-        const result = await axios.post(
-            `http://localhost:8080/api`,
-            Object.values(peopleAddresses)
-        );
-        console.log(result.data);
-        setMiddlePoint(result.data);
-    };
-
-    return (
-        <div className="App">
-            <AppData.Provider
-                value={{
-                    peopleAddresses,
-                    setPeopleAddresses,
-                    middlePoint,
-                    setMiddlePoint,
-                    handleChangeMiddle,
-                    handleSubmitMiddle
-                }}
-            >
-                <Nav />
-
-                <Router>
-                    <Switch>
-                        <Route exact path="/">
-                            <InputContainer />
-                        </Route>
-
-                        <Route exact path="/">
-                            <InfoContainer />
-                        </Route>
-
-                        <Route exact path="/result">
-                            <Result />
-                        </Route>
-
-                        <Route exact path="/login">
-                            <LogIn />
-                        </Route>
-
-                        <Route path="/register">
-                            <Register />
-                        </Route>
-                    </Switch>
-                </Router>
-                <Footer />
-            </AppData.Provider>
-        </div>
+  const handleSubmitMiddle = async (e) => {
+    e.preventDefault();
+    console.log(peopleAddresses);
+    const result = await axios.post(
+      `http://localhost:8080/api`,
+      Object.values(peopleAddresses)
     );
+    console.log(result.data);
+    setLat(result.data.longitude);
+    setLng(result.data.latitude);
+    console.log(lat, lng);
+  };
+
+  return (
+    <div className="App">
+      <AppData.Provider
+        value={{
+          peopleAddresses,
+          setPeopleAddresses,
+          middlePoint,
+          setMiddlePoint,
+          handleChangeMiddle,
+          handleSubmitMiddle,
+          lat,
+          lng,
+        }}
+      >
+        <Nav />
+
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <InputContainer />
+            </Route>
+
+            <Route exact path="/">
+              <InfoContainer />
+            </Route>
+
+            <Route exact path="/result">
+              <Result />
+            </Route>
+
+            <Route exact path="/login">
+              <LogIn />
+            </Route>
+
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </AppData.Provider>
+    </div>
+  );
 }
 
 export default App;
