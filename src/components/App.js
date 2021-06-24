3e7eabaa642070c5b1f6125ba87ac85dc9c751fc
Florian <bocu.alexandru@gmail.com>
2021-06-24 11:43:19 +0200

    login updating

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from "axios";
 import Nav from "./Nav";
 import InputContainer from "./InputContainer";
 import InfoContainer from "./InfoContainer";
@@ -11,73 +10,75 @@ import Result from "./Result";
 
 import "./App.css";
 import { AppData } from "../app-data-context";
+import { accessToken } from "mapbox-gl";
 
 function App() {
-    // input fields for everyone
-    const [peopleAddresses, setPeopleAddresses] = useState([]);
-    // middle point
-    const [middlePoint, setMiddlePoint] = useState("");
 
-    // saving all the input fields
-    const handleChangeMiddle = (e) => {
-        setPeopleAddresses({
-            ...peopleAddresses,
-            [e.target.name]: e.target.value,
-        });
-    };
+  // input fields for everyone
+  const [peopleAddresses, setPeopleAddresses] = useState([]);
+  // middle point
+  const [middlePoint, setMiddlePoint] = useState("");
 
-    const handleSubmitMiddle = async (e) => {
-        e.preventDefault();
-        console.log(peopleAddresses);
-        const result = await axios.post(
-            `http://localhost:8080/api`,
-            Object.values(peopleAddresses)
-        );
-        console.log(result.data);
-        setMiddlePoint(result.data);
-    };
+  // saving all the input fields
+  const handleChangeMiddle = (e) => {
+    setPeopleAddresses({
+      ...peopleAddresses,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return (
-        <div className="App">
-            <AppData.Provider
-                value={{
-                    peopleAddresses,
-                    setPeopleAddresses,
-                    middlePoint,
-                    setMiddlePoint,
-                    handleChangeMiddle,
-                    handleSubmitMiddle
-                }}
-            >
-                <Nav />
-
-                <Router>
-                    <Switch>
-                        <Route exact path="/">
-                            <InputContainer />
-                        </Route>
-
-                        <Route exact path="/">
-                            <InfoContainer />
-                        </Route>
-
-                        <Route exact path="/result">
-                            <Result />
-                        </Route>
-
-                        <Route exact path="/login">
-                            <LogIn />
-                        </Route>
-
-                        <Route path="/register">
-                            <Register />
-                        </Route>
-                    </Switch>
-                </Router>
-                <Footer />
-            </AppData.Provider>
-        </div>
+  const handleSubmitMiddle = async (e) => {
+    e.preventDefault();
+    console.log(peopleAddresses);
+    const result = await axios.post(
+      `http://localhost:8080/api`,
+      Object.values(peopleAddresses)
     );
+    console.log(result.data);
+    setMiddlePoint(result.data);
+  };
+
+  return (
+    <div className="App">
+      <AppData.Provider
+        value={{
+          peopleAddresses,
+          setPeopleAddresses,
+          middlePoint,
+          setMiddlePoint,
+          handleChangeMiddle,
+          handleSubmitMiddle,
+        }}
+      >
+        <Nav />
+
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <InputContainer />
+            </Route>
+
+            <Route exact path="/">
+              <InfoContainer />
+            </Route>
+
+            <Route exact path="/result">
+              <Result />
+            </Route>
+
+            <Route exact path="/login">
+              <LogIn />
+            </Route>
+
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </AppData.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,33 +1,53 @@
-import React, { useRef, useEffect, useState, useContext } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import { AppData } from "../app-data-context";
+import { useMapContext } from "../context/map-context";
+import InputContainer from "./InputContainer";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import useStyles from "../Layout/useStyles";
 
 mapboxgl.accessToken = process.env.REACT_APP_API_KEY;
+let marker;
 
 function Result() {
-    const {
-        middlePoint,
-    } = useContext(AppData);
+    const classes = useStyles();
+
+    const { middlePoint, lat, lng } = useMapContext();
+
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lat, setLat] = useState(53.55053178941791);
-    const [lng, setLng] = useState(9.992306454463948);
     const [zoom, setZoom] = useState(9);
-
+    
     useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: "mapbox://styles/mapbox/streets-v11",
-            center: [lng, lat],
-            zoom: zoom,
-        });
-    });
+        if (map.current) {
+            map.current.setCenter([lng, lat]);
+        } else {
+            map.current = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: "mapbox://styles/mapbox/streets-v11",
+                center: [lng, lat], // have to switch them.. why? nobody knows
+                zoom: zoom,
+            });
+            
+        }
+        if (marker) {
+            marker.setLngLat([lng, lat])
+        } else {
+            marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map.current);
+        }
+
+        // map.current.addControl(new mapboxgl.NavigationControl());
+    }, [lat, lng]);
 
     return (
         <div>
-            <div ref={mapContainer} className="map-container" />
-            <p>{middlePoint}</p>
+            <InputContainer />
+            <Container>
+                <Card className={classes.map}>
+                    <div ref={mapContainer} className="map-container" />
+                </Card>
+            </Container>
+            {/* <p>{middlePoint}</p> */}
         </div>
     );
 }
