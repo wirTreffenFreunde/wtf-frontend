a0e7d7dbf60f604ed61bbb421c57afdb915718f8
ilona <ilona.avdiushkina@gmail.com>
2021-07-01 12:11:15 +0200

    merging

--- src/Layout/useStyles.js
+++ src/Layout/useStyles.js
@@ -1,6 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
@@ -37,7 +44,6 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     backgroundColor: theme.palette.background.paper,
   },
-<<<<<<< HEAD
   tripButton: {
     width: "100%",
     backgroundColor: theme.palette.background.paper,
@@ -52,80 +58,66 @@ const useStyles = makeStyles((theme) => ({
   popOvertypography: {
     padding: theme.spacing(2),
   },
-  "@global": {
-    ul: {
-      margin: 0,
-      padding: 0,
-      listStyle: "none",
-=======
-    "@global": {
-        ul: {
-            margin: 0,
-            padding: 0,
-            listStyle: "none",
-        },
-    },
-    appBar: {
-        borderBottom: `1px solid ${theme.palette.divider}`,
-    },
-    toolbar: {
-        flexWrap: "wrap",
-    },
-    toolbarTitle: {
-        flexGrow: 1,
-    },
-    link: {
-        margin: theme.spacing(1, 1.5),
-    },
-    card: {
-        margin: theme.spacing(3),
-        padding: theme.spacing(3),
-        // height: "300px",
-    },
-    cardMap: {
-        margin: theme.spacing(3),
-        height: "500px",
-    },
-    mapPopup: {
-        margin: theme.spacing(0),
-        padding: theme.spacing(0),
-        backgroundColor: "black"
-    },
-    mainForm: {},
-    inputField: {
-        width: "300px",
-    },
-    submitForm: {
-        // width: "300px"
-    },
-    footerContainer: {
-        // backgroundColor: "#212121",
-        // margin: theme.spacing(0),
-    },
-    footer: {
-        borderTop: `1px solid ${theme.palette.divider}`,
-        marginTop: theme.spacing(8),
-        paddingTop: theme.spacing(3),
-        paddingBottom: theme.spacing(3),
-    },
-    paper: {
-        marginTop: theme.spacing(8),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: "100%", //Fix IE 11 issue.
-        marginTop: theme.spacing(3, 0, 2),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
->>>>>>> upstream/main
-    },
+
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  card: {
+    margin: theme.spacing(3),
+    padding: theme.spacing(3),
+    // height: "300px",
+  },
+  cardMap: {
+    margin: theme.spacing(3),
+    height: "500px",
+  },
+  mapPopup: {
+    margin: theme.spacing(0),
+    padding: theme.spacing(0),
+    backgroundColor: "black",
+  },
+  mainForm: {},
+  inputField: {
+    width: "300px",
+  },
+  submitForm: {
+    // width: "300px"
+  },
+  footerContainer: {
+    // backgroundColor: "#212121",
+    // margin: theme.spacing(0),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", //Fix IE 11 issue.
+    marginTop: theme.spacing(3, 0, 2),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
   },
   appBar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
