103937cdf4cf99dfe35b3e2d59dcac8880bfa166
Florian <bocu.alexandru@gmail.com>
2021-07-29 13:34:35 +0200

    changed react-map-gl

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -12,10 +12,7 @@ function AboutContainer() {
       <Card className={classes.homeCard}>
         <Typography variant="h3" className={classes.h3}>About us</Typography>
         <Typography variant="body" className={classes.bodyText}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio,
-          molestias dolore! Molestias exercitationem ut quibusdam maiores illo
-          aperiam hic voluptas recusandae quidem soluta repellendus aut, ea
-          earum fugit vitae explicabo.
+        We are a team of 5 Junior Web Developer who has been working on their  Project to build WirTreffenFreunde App for friends or acquaintances to meet up to their middle point. The Project has also a user account where you can store and save your next destination with points of interest like the closest city, restaurants, or hotels. The whole trip can have up to 5 members.
         </Typography>
       </Card>
     </Container>
